ebe661be9050911c0d47a67ad4dcb069a351667d
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-10-19 11:56:43 -0300

    Separação por configuração e implementação por assembly em uma linha

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,3 +1,4 @@
+using CursoEFCore.Data.Configurations;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,49 +15,7 @@ namespace CursoEFCore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(p =>
-            {
-                p.ToTable("Clientes");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Nome).HasColumType('VARCHAR(80)').IsRequired();
-                p.Property(p => p.Telefone).HasColumType('CHAR(11)');
-                p.Property(p => p.CEP).HasColumType('CHAR(8)').IsRequired();
-                p.Property(p => p.Estado).HasColumType('CHAR(2)').IsRequired();
-                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
-
-                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
-            });
-            
-            modelBuilder.Entity<Produto>(p =>
-            {
-                p.ToTable("Produtos");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.CodigoBarras).HasColumType('VARCHAR(14)').IsRequired();
-                p.Property(p => p.Descricao).HasColumType('VARCHAR(60)');
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.TipoProduto).HasConversion<string>();
-            });
-
-            modelBuilder.Entity<Pedido>(p =>
-            {
-                p.ToTable("Pedidos");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGenerateOnAdd();
-                p.Property(p => p.StatusPedido).HasConversion<string>();
-                p.Property(p => p.TipoFrete).HasConversion<string>();
-                p.Property(p => p.Observacao).HasColumType('VARCHAR(512)');
-
-                p.HasMany(p => p.Itens).WithOne(p => p.Pedido).OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<PedidoItem>(p =>
-            {
-                p.ToTable("PedidoItens");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Quantidade).HasDefaultValueSql(1).IsRequired();
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.Desconto).IsRequired();
-            });
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicaionContext).Assembly);
         }
     }
 }
---
+++ Curso/Data/Configurations/PedidoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CursoEFCore.Data.Configurations
+{
+    class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.ToTable("Pedidos");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGenerateOnAdd();
+            builder.Property(p => p.StatusPedido).HasConversion<string>();
+            builder.Property(p => p.TipoFrete).HasConversion<string>();
+            builder.Property(p => p.Observacao).HasColumType('VARCHAR(512)');
+
+            builder.HasMany(p => p.Itens).WithOne(p => p.Pedido).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
